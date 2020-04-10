c522b58dd2640912a5b9070bb0880480f8f38c3b
mr1dha <muhammadridha11@gmail.com>
2020-04-11 00:18:41 +0700

    Menambahkan image slide pada halaman Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,127 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="{{asset('css/style.css')}}">
 	<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('css/fontawesome-free/css/all.min.css')}}">
 	<title>Home</title>
 </head>
 <body>
-	<h1>Home Page</h1>
-	
+	<!-- NAVBAR -->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-transparent fixed-top">
+	<div class="container">
+	  <a class="navbar-brand font-weight-bold text-white mb-3" href="#"><i class="text-warning fa-2x fas fa-cloud-meatball p-0"></i>&nbsp; KULINERIAN</a>
+	  <button class="navbar-toggler border" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarNav">
+	    <ul class="navbar-nav ml-auto">
+	       <li class="nav-item dropdown ">
+	        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          KATEGORI 1
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+	          <a class="dropdown-item" href="#">Action</a>
+	          <a class="dropdown-item" href="#">Another action</a>
+	          <a class="dropdown-item" href="#">Something else here</a>
+	        </div>
+	      </li>
+	      <li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          KATEGORI 2
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+	          <a class="dropdown-item" href="#">Action</a>
+	          <a class="dropdown-item" href="#">Another action</a>
+	          <a class="dropdown-item" href="#">Something else here</a>
+	        </div>
+	      </li>
+	      <li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          KATEGORI 3
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+	          <a class="dropdown-item" href="#">Action</a>
+	          <a class="dropdown-item" href="#">Another action</a>
+	          <a class="dropdown-item" href="#">Something else here</a>
+	        </div>
+	      </li>
+	      <li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          KATEGORI 4
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+	          <a class="dropdown-item" href="#">Action</a>
+	          <a class="dropdown-item" href="#">Another action</a>
+	          <a class="dropdown-item" href="#">Something else here</a>
+	        </div>
+	      </li>
+	    </ul>
+	  </div>
+	  </div>
+	</nav>
+
+	<!-- JUMBOTRON -->
+	<div class="row row-slide">
+		<div class="col-12">
+			<div class="jumbotron bg-transparent">
+		<div class="container">
+		  <h1 class="display-4">TEMUKAN SELERA KULINERMU DI <br></h1>
+		  <hr class="my-4">
+		  <span class="web-title">KULINERIAN</span>
+		 <!-- <p class="lead">Website portal kuliner Aceh terlengkap</p> -->
+		  
+		  <form action="" method="POST">
+		  	<div class="form-group mt-5">
+				<div class="input-group">
+					<!-- <div class="input-group-prepend">
+						<div class="input-group-text"><i class="fas fa-unlock-alt"></i></div>
+					</div> -->
+					<input type="text" class="search-bar" name="cari" placeholder="Mau cari kuliner apa?">
+				</div>
+			</div>
+		  	<a class="btn btn-warning btn-lg" href="#" role="button">CARI</a>
+		  </form>
+		</div>
+	</div>
+		</div>
+	</div>
+
+	<!-- SLIDE IMAGE -->
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+	  <ol class="carousel-indicators">
+	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	  </ol>
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <img src="{{asset('img/bg1.png')}}" class="d-block w-100" alt="...">
+	      <div class="carousel-caption d-none d-md-block">
+	        <h5>First slide label</h5>
+	        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+	      </div>
+	    </div>
+	    <div class="carousel-item">
+	      <img src="{{asset('img/bg2.png')}}" class="d-block w-100" alt="...">
+	    </div>
+	    <div class="carousel-item">
+	      <img src="{{asset('img/bg1.png')}}" class="d-block w-100" alt="...">
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<script src="{{asset('assets/js/script.js')}}"></script>
 </body>
 </html>
